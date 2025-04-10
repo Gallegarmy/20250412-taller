a459b63bb0f70a337b6d9a2be66496c59c143b7e
madtyn <madtyn@gmail.com>
2025-04-10 14:59:07 +0200

    Formats the result. Refactors.

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ class Scraper:
     async def get_page(self, url: str) -> Page:
         page = await (await self._get_browser()).new_page()
         await page.goto(url)
-        #await sleep(1.6)
+        await sleep(1.6)
+        #await page.wait_for_load_state('domcontentloaded')  # alternativa: 'load'
         return page
 
 
@@ -29,23 +30,30 @@ class Scraper:
 async def main():
     async with async_playwright() as playwright:
         scraper = Scraper(playwright)
-        page = await scraper.get_page('https://dinahosting.com/hosting/precios-hosting')
-        await page.wait_for_load_state('load')
+        url = 'https://gl.dinahosting.com/hosting/precios-hosting'  # 'https://dinahosting.com/hosting/precios-hosting'
+        page = await scraper.get_page(url)
 
         for selector in SELECTORS:
             button_elems = await page.query_selector_all(selector + ' button')
             ul_elems = await page.query_selector_all(selector + ' ul')
             while(len(ul_elems)):
                 title_button = button_elems.pop()
-                title_text = await title_button.text_content()
-                print(f"***** {SPACE_REGEX.sub(' ', title_text)}", end='')
+                await process_title(title_button)
 
                 ul_elem = ul_elems.pop()
                 li_elems = await ul_elem.query_selector_all('li')
                 for li_elem in li_elems:
-                    elem_text = await li_elem.text_content()
-                    print(SPACE_REGEX.sub(' ', elem_text))
+                    duration_elem = await li_elem.query_selector('span:nth-child(1)')
+                    price_elem = await li_elem.query_selector('p')
+                    print(f'{await get_clean_text(price_elem):>12} :: {await get_clean_text(duration_elem)}')
 
+
+async def process_title(title_button):
+    print(f"\n***** {await get_clean_text(title_button)}")
+
+async def get_clean_text(elem):
+    text = await elem.text_content()
+    return SPACE_REGEX.sub(' ', text)
 
 if __name__ == '__main__':
     asyncio.run(main())
